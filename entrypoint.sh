5a8e7b4874ad032a26c4a49fe24bbf3e249aba05
kopfan <34918435+kopfan@users.noreply.github.com>
2024-02-17 18:27:45 +0100

    Aktualisieren von entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,8 @@ echo "::set-output name=time::$time"
 b4a version
 echo "I am: " $(whoami)
 echo "my folder: " $(pwd)
+echo "home folder: " $(home)
 #ls -laR
 echo "#############"
 #b4a list
-ls -da .
+ls -da /
